5950516292e403bea95b56076e291d6e69afe964
EnricoGenovese <enricogeno@libero.it>
2025-01-10 16:25:04 +0100

    add tags in create post;

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 
+import { tags } from "../data/tags";
+
 const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
 const postsEndPoint = "/posts/";
 
@@ -20,13 +22,31 @@ export default function AddPost() {
         setFormData({ ...formData, [event.target.name]: value })
     }
 
+    function handleTags(e) {
+        setFormData((formData) => {
+            let { tags, ...others } = formData;
+            tags.includes(e.target.value) ?
+                tags = tags.filter((val) => val !== e.target.value) :
+                tags = [...tags, e.target.value];
+            return {
+                tags,
+                ...others,
+            };
+        });
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        // newId = posts.reduce((curr, next) => curr.id < next.id ? next : curr).id + 1
+        // newId = formData.reduce((curr, next) => curr.id < next.id ? next : curr).id + 1
         axios.post(apiUrl + postsEndPoint, formData)
             .then((res) => {
                 console.log(res.data);
-
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                setFormData(newPost);
             })
     }
 
@@ -68,16 +88,24 @@ export default function AddPost() {
                         onChange={handleInput}
                         name="img"
                         required />
-                    <label htmlFor="tags">Tags del post</label>
-                    <input
-                        type="text"
-                        className="form-control my-2"
-                        id="tags"
-                        aria-describedby="tagsHelp"
-                        value={formData.tags}
-                        onChange={handleInput}
-                        name="tags"
-                        required />
+                    <div className="card p-4 d-flex flex-wrap justify-content-between">
+                        {tags.map((tag) => (
+                            <div className="mb-3 form-check" key={tag.id}>
+                                <input
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    id="tags"
+                                    name="tags"
+                                    onChange={handleTags}
+                                    value={tag.title}
+                                    checked={formData.tags.includes(tag.title)}
+                                />
+                                <label className="form-check-label" htmlFor="avaiable">
+                                    {tag.title}
+                                </label>
+                            </div>
+                        ))}
+                    </div>
                     <button type="submit" className="btn btn-success my-3">
                         Pubblica
                     </button>
